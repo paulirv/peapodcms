0abc3d03f09fc8d58517c9114aff8dc49c1ba4f4
paulirv <paul@iwpi.com>
2025-01-10 18:14:58 -0800

    feat: enhance webpack configuration with CopyWebpackPlugin and buffer fallback

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development', // Explicitly set mode to 'development' for now
@@ -25,14 +27,34 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    fallback: {
+      buffer: require.resolve('buffer/'),
+      path: require.resolve('path-browserify'),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'], // Add the Buffer global object
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/content', to: 'content' }, // Copy content to dist
+      ],
+    }),
   ],
   devServer: {
-    static: './dist',
+    static: [
+      {
+        directory: path.join(__dirname, 'public'),
+      },
+      {
+        directory: path.join(__dirname, 'dist'),
+      },
+    ],
+    historyApiFallback: true, // Ensures all routes serve index.html
     hot: true,
   },
 };
